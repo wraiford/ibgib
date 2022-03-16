daf54b9511197110a9edac2e66a17eda7126f251
William Raiford <bill.mybiz@gmail.com>
2022-03-16 09:48:26 -0500

    Removed unused browser-gib and webext-gib projects
    * both of these were used as trials for what is now in ionic-gib.

--- webext-gib/src/app/common/bases/ibgib-component-base.spec.ts
+++
@@ -1,7 +0,0 @@
-import { IbgibComponentBase } from './ibgib-component-base';
-
-describe('IbgibComponentBase', () => {
-  it('should create an instance', () => {
-    expect(new IbgibComponentBase()).toBeTruthy();
-  });
-});
--- webext-gib/src/app/common/bases/ibgib-list-component-base.spec.ts
+++
@@ -1,7 +0,0 @@
-import { IbgibListComponentBase } from './ibgib-list-component-base';
-
-describe('IbgibListComponentBase', () => {
-  it('should create an instance', () => {
-    expect(new IbgibListComponentBase()).toBeTruthy();
-  });
-});
