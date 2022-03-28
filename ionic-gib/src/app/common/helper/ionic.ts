18ead89384a7c8189654cf83194cadda538e5eb0
William Raiford <bill.mybiz@gmail.com>
2022-03-28 08:17:03 -0500

    InProgress: troubleshooting download pic, adding to ionic helpers
    * added helper

--- ionic-gib/src/app/common/helper/ionic.ts
+++ ionic-gib/src/app/common/helper/ionic.ts
@@ -1,5 +1,5 @@
 import {
-    Plugins, FilesystemEncoding, FileReadResult, FilesystemDirectory, Capacitor
+    Plugins, FilesystemEncoding, FileReadResult, FilesystemDirectory, Capacitor, FileWriteResult
 } from '@capacitor/core';
 const { Filesystem } = Plugins;
 
@@ -66,4 +66,141 @@ export async function pathExists({
     } finally {
         if (logalot) { console.log(`${lc} complete.`); }
     }
+}
+
+
+export async function writeFile({
+    path,
+    data,
+    directory,
+    encoding,
+}: {
+    path: string,
+    data: string,
+    directory: FilesystemDirectory,
+    encoding?: FilesystemEncoding,
+}): Promise<FileWriteResult> {
+    const lc = `[${writeFile.name}]`;
+    try {
+        if (logalot) { console.log(`${lc} starting...`); }
+        if (!path) { throw new Error(`path required (E: dd39a1a226f26e1675a3a99dea3b2e22)`); }
+        if (!data) { throw new Error(`data required (E: f2cf8ad9d118eaeaba9982a5395bcc22)`); }
+        if (!directory) { throw new Error(`directory required (E: 71897de292b2a1ff219d9ddb01855722)`); }
+
+        encoding = encoding || FilesystemEncoding.UTF8;
+
+        const resWrite = await Filesystem.writeFile({
+            path,
+            data,
+            directory,
+            encoding: FilesystemEncoding.UTF8,
+        });
+
+        if (logalot) { console.log(`${lc} resWrite?.uri: ${resWrite?.uri} (I: 85e54f5172153f2c1fc7d54dda44a122)`); }
+        return resWrite;
+    } catch (error) {
+        console.error(`${lc} ${error.message}`);
+        throw error;
+    } finally {
+        if (logalot) { console.log(`${lc} complete.`); }
+    }
+}
+
+/**
+ * Checks to see if `dirPath` exists, if not makes it.
+ */
+export async function ensureDirPath({
+    dirPath,
+    directory,
+}: {
+    dirPath: string,
+    directory: FilesystemDirectory,
+}): Promise<void> {
+    const lc = `[${ensureDirPath.name}]`;
+    try {
+        if (logalot) { console.log(`${lc} starting...`); }
+        const exists = await dirPathExists({dirPath, directory});
+        if (!exists) { await mkdir({dirPath, directory}); }
+    } catch (error) {
+        console.error(`${lc} ${error.message}`);
+        throw error;
+    } finally {
+        if (logalot) { console.log(`${lc} complete.`); }
+    }
+}
+
+/**
+ *
+ * @returns true if `dirPath` exists, else false.
+ *
+ * ## notes
+ *
+ * * if an exception occurs, this may be masked by a false result but will be
+ *   logged if logalot.
+ */
+export async function dirPathExists({
+    dirPath,
+    directory,
+}: {
+    dirPath: string,
+    directory: FilesystemDirectory,
+}): Promise<boolean> {
+    const lc = `[${dirPathExists.name}]`;
+    try {
+        if (logalot) { console.log(`${lc} starting... path: ${dirPath}, directory: ${directory} (I: 5d6c290cdc7ad2f89326477775ebcd22)`); }
+
+        await Filesystem.readdir({ path: dirPath, directory });
+        if (logalot) { console.log(`${lc} YES, did exist (I: 0045ed20d4ae4a48a412bb2bc60984cb)`); }
+        return true;
+    } catch (error) {
+        if (logalot) { console.log(`${lc} NO, did NOT exist (I: 0045ed20d4ae4a48a412bb2bc60984cb)`); }
+        return false;
+    } finally {
+        if (logalot) { console.log(`${lc} complete.`); }
+    }
+}
+
+export async function mkdir({
+    dirPath,
+    directory,
+}: {
+    dirPath: string,
+    directory: FilesystemDirectory,
+}): Promise<void> {
+    const lc = `[${mkdir.name}]`;
+    try {
+        if (logalot) { console.log(`${lc} starting...`); }
+        await Filesystem.mkdir({ path: dirPath, directory, recursive: true });
+    } catch (error) {
+        console.error(`${lc} error creating path (${dirPath}) and directory (${directory}). error: ${error.message} (E: e557ebc6885d4b74a393ad80b543abc4)`);
+        throw error;
+    } finally {
+        if (logalot) { console.log(`${lc} complete.`); }
+    }
+}
+
+export function getBlob (b64Data: string): Blob {
+    let contentType = '';
+    let sliceSize = 512;
+
+    // b64Data = b64Data.replace(/data\:image\/(jpeg|jpg|png)\;base64\,/gi, '');
+
+    let byteCharacters = atob(b64Data);
+    // let byteCharacters = Buffer.from(b64Data, 'base64');
+    let byteArrays = [];
+
+    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+      const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+      const byteNumbers = new Array(slice.length);
+      for (let i = 0; i < slice.length; i++) {
+          byteNumbers[i] = slice.charCodeAt(i);
+      }
+
+      let byteArray = new Uint8Array(byteNumbers);
+      byteArrays.push(byteArray);
+    }
+
+    let blob = new Blob(byteArrays, {type: contentType});
+    return blob;
 }
