5ac8d9e7fc8871d93d9f6311fe5fe964d920e1d9
William Raiford <bill.mybiz@gmail.com>
2022-05-27 12:07:36 -0500

    fix: android light/dark mode no longer ignored
    * there was a breaking change in android 10 apparently
    * https://github.com/ionic-team/capacitor/discussions/1978#discussioncomment-708439

--- ionic-gib/android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ ionic-gib/android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,6 +1,10 @@
+// @see https://github.com/ionic-team/capacitor/discussions/1978#discussioncomment-708439
+
 package io.ionic.starter;
 
 import android.os.Bundle;
+import android.content.res.Configuration;
+import android.webkit.WebSettings;
 
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
@@ -8,6 +12,7 @@ import com.getcapacitor.Plugin;
 import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -17,5 +22,36 @@ public class MainActivity extends BridgeActivity {
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
     }});
+  }
+  @Override
+  public void onStart() {
+    super.onStart();
+    setDarkMode();
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    setDarkMode();
+  }
+
+  void setDarkMode() {
+    // Android "fix" for enabling dark mode
+    // @see: https://github.com/ionic-team/capacitor/discussions/1978
+    int nightModeFlags = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+    WebSettings webSettings = this.bridge.getWebView().getSettings();
+    if (nightModeFlags == Configuration.UI_MODE_NIGHT_YES) {
+      if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+        // As of Android 10, you can simply force the dark mode
+        webSettings.setForceDark(WebSettings.FORCE_DARK_ON);
+      }
+      // Before Android 10, we need to use a CSS class based fallback
+      this.bridge.getWebView().evaluateJavascript("document.body.classList.toggle('dark', true);", null);
+    } else {
+      if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+        webSettings.setForceDark(WebSettings.FORCE_DARK_OFF);
+      }
+      this.bridge.getWebView().evaluateJavascript("document.body.classList.toggle('dark', false);", null);
+    }
   }
 }
