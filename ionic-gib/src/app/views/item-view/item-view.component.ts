dd7d7e954ce383309a8f9db813cca77baf8b2fee
William Raiford <bill.mybiz@gmail.com>
2022-11-13 06:29:57 -0600

    impl: pic label for link ibgibs
    * now shows the first child pic rel8d to links as a "pic label" for that
      link.

--- ionic-gib/src/app/views/item-view/item-view.component.ts
+++ ionic-gib/src/app/views/item-view/item-view.component.ts
@@ -26,95 +26,12 @@ const debugBorder = c.GLOBAL_DEBUG_BORDER || false;
   styleUrls: ['./item-view.component.scss'],
 })
 export class ItemViewComponent extends IbgibComponentBase<IbGibListItem> {
-  private _loadingLabelAddr: boolean;
-  async loadLabelAddr(): Promise<void> {
-    const lc = `${this.lc}[${this.loadLabelAddr.name}]`;
-    if (this._loadingLabelAddr) {
-      if (logalot) { console.log(`${lc} already loading label addr. returning early. (I: 91d47bcc4e381f48189f65cd352ce822)`); }
-      return; /* <<<< returns early */
-    }
-    this._loadingLabelAddr = true;
-    try {
-      if (logalot) { console.log(`${lc} starting... (I: 753cd7157629a3ce05a9e7cb20867c22)`); }
-
-      let tries = 0;
-      while (!this.ibGib) { // arbitrary 30 seconds?
-        if (logalot) { console.log(`${lc} this.ibGib falsy. checking again in a second (I: cd496f9bf67045d58d0502e0e177b5d9)`); }
-        await h.delay(1000);
-        tries++;
-        if (tries >= 30) {
-          if (logalot) { console.log(`${lc} this.ibGib falsy. returning early. (I: f138f328717b7de4ea145e48ee802122)`); }
-        }
-      }
-
-      if (this.labelAddr) {
-        if (logalot) { console.log(`${lc} this.labelAddr already set. returning early. (I: 0ff17253f6bc561aa656b4060d0e0222)`); }
-        return; /* <<<< returns early */
-      }
-
-      if (this.item?.type !== 'link' && this.item?.type !== 'pic') {
-        if (logalot) { console.log(`${lc} only showing labels for links and pics right now, hardcoded kluge. returning early (I: 3792a7cab158ca1651241776f9b90322)`); }
-        return; /* <<<< returns early */
-      }
-
-      const commentAddrs = this.ibGib.rel8ns?.comment ?? [];
-      if (commentAddrs.length === 0) {
-        if (logalot) { console.log(`${lc} no comment addrs linked to ibGib. returning early. (I: 9f1b92200013e679e9277302c6e16822)`); }
-        return; /* <<<< returns early */
-      }
-
-      let labelAddr: IbGibAddr;
-      let labelIbGib: CommentIbGib_V1;
-      for (let i = 0; i < commentAddrs.length; i++) {
-        const commentAddr = commentAddrs[i];
-        let resGet = await this.common.ibgibs.get({ addr: commentAddr });
-        if (resGet.success && resGet.ibGibs?.length === 1 && resGet.ibGibs[0].data?.text) {
-          let latestCommentAddr = await this.common.ibgibs.getLatestAddr({ ibGib: resGet.ibGibs[0] });
-          if (latestCommentAddr && latestCommentAddr !== commentAddr) {
-            const resGetLatest = await this.common.ibgibs.get({ addr: latestCommentAddr });
-            if (resGetLatest.success && resGetLatest.ibGibs?.length === 1 && resGetLatest.ibGibs[0].data?.text) {
-              labelIbGib = <CommentIbGib_V1>resGetLatest.ibGibs[0];
-              labelAddr = latestCommentAddr;
-            } else {
-              // some kind of error, so use best get
-              console.error(`${lc} problem getting the latest. (E: 2aa478fcd5a048af9453b68a0ae640b4)`)
-              labelIbGib = <CommentIbGib_V1>resGet.ibGibs[0];
-              labelAddr = commentAddr;
-            }
-          } else {
-            // no latest or it's the same, so just use what we got
-            labelIbGib = <CommentIbGib_V1>resGet.ibGibs[0];
-            labelAddr = commentAddr;
-          }
-          break;
-        } else {
-          console.error(`${lc} there was a problem loading a child comment for the label? trying next. addr: ${commentAddr}. error: ${resGet.errorMsg ?? 'some error'} (E: 66e9fa8d159842059bf34efe084a2924)`)
-        }
-      }
-
-      if (labelAddr && labelIbGib) {
-        this.labelAddr = labelAddr;
-        this.labelText = labelIbGib.data.text;
-      } else {
-        console.error(`${lc} couldn't load the label for some reason. labelIbGib falsy. (E: 9c51525467d54dbc8743d7242e1b8a0b)`);
-        delete this.labelAddr;
-        delete this.labelText;
-      }
-    } catch (error) {
-      console.error(`${lc} ${error.message}`);
-      throw error;
-    } finally {
-      setTimeout(() => this.ref.detectChanges());
-      this._loadingLabelAddr = false;
-      if (logalot) { console.log(`${lc} complete.`); }
-    }
-  }
-
   protected lc: string = `[${ItemViewComponent.name}]`;
   public debugBorderWidth: string = debugBorder ? "1px" : "0px"
   public debugBorderColor: string = "#92ed80";
   public debugBorderStyle: string = "solid";
 
+  private _loadingLabelAddr: boolean;
 
   @Input()
   get checked(): boolean {
@@ -241,6 +158,89 @@ export class ItemViewComponent extends IbgibComponentBase<IbGibListItem> {
       console.error(`${lc} ${error.message}`);
       throw error;
     } finally {
+      if (logalot) { console.log(`${lc} complete.`); }
+    }
+  }
+
+  async loadLabelAddr(): Promise<void> {
+    const lc = `${this.lc}[${this.loadLabelAddr.name}]`;
+    if (this._loadingLabelAddr) {
+      if (logalot) { console.log(`${lc} already loading label addr. returning early. (I: 91d47bcc4e381f48189f65cd352ce822)`); }
+      return; /* <<<< returns early */
+    }
+    this._loadingLabelAddr = true;
+    try {
+      if (logalot) { console.log(`${lc} starting... (I: 753cd7157629a3ce05a9e7cb20867c22)`); }
+
+      let tries = 0;
+      while (!this.ibGib) { // arbitrary 30 seconds?
+        if (logalot) { console.log(`${lc} this.ibGib falsy. checking again in a second (I: cd496f9bf67045d58d0502e0e177b5d9)`); }
+        await h.delay(1000);
+        tries++;
+        if (tries >= 30) {
+          if (logalot) { console.log(`${lc} this.ibGib falsy. returning early. (I: f138f328717b7de4ea145e48ee802122)`); }
+        }
+      }
+
+      if (this.labelAddr) {
+        if (logalot) { console.log(`${lc} this.labelAddr already set. returning early. (I: 0ff17253f6bc561aa656b4060d0e0222)`); }
+        return; /* <<<< returns early */
+      }
+
+      if (this.item?.type !== 'link' && this.item?.type !== 'pic') {
+        if (logalot) { console.log(`${lc} only showing labels for links and pics right now, hardcoded kluge. returning early (I: 3792a7cab158ca1651241776f9b90322)`); }
+        return; /* <<<< returns early */
+      }
+
+      const commentAddrs = this.ibGib.rel8ns?.comment ?? [];
+      if (commentAddrs.length === 0) {
+        if (logalot) { console.log(`${lc} no comment addrs linked to ibGib. returning early. (I: 9f1b92200013e679e9277302c6e16822)`); }
+        return; /* <<<< returns early */
+      }
+
+      let labelAddr: IbGibAddr;
+      let labelIbGib: CommentIbGib_V1;
+      for (let i = 0; i < commentAddrs.length; i++) {
+        const commentAddr = commentAddrs[i];
+        let resGet = await this.common.ibgibs.get({ addr: commentAddr });
+        if (resGet.success && resGet.ibGibs?.length === 1 && resGet.ibGibs[0].data?.text) {
+          let latestCommentAddr = await this.common.ibgibs.getLatestAddr({ ibGib: resGet.ibGibs[0] });
+          if (latestCommentAddr && latestCommentAddr !== commentAddr) {
+            const resGetLatest = await this.common.ibgibs.get({ addr: latestCommentAddr });
+            if (resGetLatest.success && resGetLatest.ibGibs?.length === 1 && resGetLatest.ibGibs[0].data?.text) {
+              labelIbGib = <CommentIbGib_V1>resGetLatest.ibGibs[0];
+              labelAddr = latestCommentAddr;
+            } else {
+              // some kind of error, so use best get
+              console.error(`${lc} problem getting the latest. (E: 2aa478fcd5a048af9453b68a0ae640b4)`)
+              labelIbGib = <CommentIbGib_V1>resGet.ibGibs[0];
+              labelAddr = commentAddr;
+            }
+          } else {
+            // no latest or it's the same, so just use what we got
+            labelIbGib = <CommentIbGib_V1>resGet.ibGibs[0];
+            labelAddr = commentAddr;
+          }
+          break;
+        } else {
+          console.error(`${lc} there was a problem loading a child comment for the label? trying next. addr: ${commentAddr}. error: ${resGet.errorMsg ?? 'some error'} (E: 66e9fa8d159842059bf34efe084a2924)`)
+        }
+      }
+
+      if (labelAddr && labelIbGib) {
+        this.labelAddr = labelAddr;
+        this.labelText = labelIbGib.data.text;
+      } else {
+        console.error(`${lc} couldn't load the label for some reason. labelIbGib falsy. (E: 9c51525467d54dbc8743d7242e1b8a0b)`);
+        delete this.labelAddr;
+        delete this.labelText;
+      }
+    } catch (error) {
+      console.error(`${lc} ${error.message}`);
+      throw error;
+    } finally {
+      setTimeout(() => this.ref.detectChanges());
+      this._loadingLabelAddr = false;
       if (logalot) { console.log(`${lc} complete.`); }
     }
   }
--- ionic-gib/src/app/views/link-view/link-view.component.ts
+++ ionic-gib/src/app/views/link-view/link-view.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit, ChangeDetectorRef, Input, ChangeDetectionStrategy } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 import * as h from 'ts-gib/dist/helper';
 import { IbGibAddr } from 'ts-gib';
-import { IbGib_V1 } from 'ts-gib/dist/V1';
 
-import { IbgibComponentBase } from 'src/app/common/bases/ibgib-component-base';
-import { CommonService } from 'src/app/services/common.service';
 import * as c from '../../common/constants';
+import { CommonService } from '../../services/common.service';
+import { IbgibComponentBase } from '../../common/bases/ibgib-component-base';
 import { YOUTUBE_LINK_REG_EXP } from '../../common/constants';
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { PicData_V1, PicIbGib_V1 } from '../../common/types/pic';
+import { IbGibItem } from 'src/app/common/types/ux';
 
 const logalot = c.GLOBAL_LOG_A_LOT || false;
 const debugBorder = c.GLOBAL_DEBUG_BORDER || false;
@@ -25,17 +26,20 @@ export class LinkViewComponent
 
   protected lc: string = `[${LinkViewComponent.name}]`;
 
-  @Input()
-  get addr(): IbGibAddr { return super.addr; }
-  set addr(value: IbGibAddr) { super.addr = value; }
-
-  @Input()
-  get ibGib_Context(): IbGib_V1 { return super.ibGib_Context; }
-  set ibGib_Context(value: IbGib_V1) { super.ibGib_Context = value; }
-
   public debugBorderWidth: string = debugBorder ? "5px" : "0px"
   public debugBorderColor: string = "yellow";
   public debugBorderStyle: string = "solid";
+
+  private _loadingPicLabelAddr: boolean;
+
+  @Input()
+  picLabelItem: IbGibItem;
+
+  @Input()
+  get picLabelAddr(): IbGibAddr { return this.picLabelItem?.addr; }
+
+  @Input()
+  get picLabelSrc(): string { return this.picLabelItem?.picSrc; }
 
   /**
    * redeclared for view
@@ -102,6 +106,7 @@ export class LinkViewComponent
       await this.loadIbGib();
       await this.loadTjp();
       await this.loadItem();
+      await this.loadPicLabel();
       // trigger an initial ping to check for newer ibgibs
       if (!this.paused) {
         setTimeout(async () => {
@@ -115,6 +120,111 @@ export class LinkViewComponent
     } finally {
       this.ref.detectChanges();
       if (logalot) { console.log(`${lc} updated.`); }
+    }
+  }
+
+
+  /**
+   * "pic label" is the picture that is the representative pic of the link. It
+   * is populated with the first child pic of the link (first pic ibGib with
+   * addr, if any, in `this.ibGib.rel8ns.pic` array).
+   */
+  async loadPicLabel(): Promise<void> {
+    const lc = `${this.lc}[${this.loadPicLabel.name}]`;
+
+    if (this._loadingPicLabelAddr) {
+      if (logalot) { console.log(`${lc} already loading pic label addr. returning early. (I: 45754e464e044e248ecb455527c614de)`); }
+      return; /* <<<< returns early */
+    }
+
+    this._loadingPicLabelAddr = true;
+    try {
+      if (logalot) { console.log(`${lc} starting... (I: c1d8329e074341618eeb9845f0f26107)`); }
+
+      let tries = 0;
+      while (!this.ibGib) { // arbitrary 30 seconds?
+        if (logalot) { console.log(`${lc} this.ibGib falsy. checking again in a second (I: 321065b501334348bff72cd5bee92a9e)`); }
+        await h.delay(1000);
+        tries++;
+        if (tries >= 30) {
+          if (logalot) { console.log(`${lc} this.ibGib falsy. returning early. (I: a69678234bce4443b2bd9560657559ee)`); }
+        }
+      }
+
+      if (this.picLabelAddr) {
+        if (logalot) { console.log(`${lc} this.labelAddr already set. returning early. (I: a91d5a40fb44407c8290af5477d2a463)`); }
+        return; /* <<<< returns early */
+      }
+
+      const picAddrs = this.ibGib.rel8ns?.pic ?? [];
+      if (picAddrs.length === 0) {
+        if (logalot) { console.log(`${lc} no pic addrs linked to ibGib. returning early. (I: 4bb5ea95f29e437089b7fa0ebc6f1c96)`); }
+        return; /* <<<< returns early */
+      }
+
+      let picLabelAddr: IbGibAddr;
+      let picLabelIbGib: PicIbGib_V1;
+
+      // get the latest address for the pic
+      for (let i = 0; i < picAddrs.length; i++) {
+        const picAddr = picAddrs[i];
+        let resGet = await this.common.ibgibs.get({ addr: picAddr });
+        if (resGet.success && resGet.ibGibs?.length === 1 && (<PicData_V1>resGet.ibGibs[0].data)?.binHash) {
+          let latestpicAddr = await this.common.ibgibs.getLatestAddr({ ibGib: resGet.ibGibs[0] });
+          if (latestpicAddr && latestpicAddr !== picAddr) {
+            const resGetLatest = await this.common.ibgibs.get({ addr: latestpicAddr });
+            if (resGetLatest.success && resGetLatest.ibGibs?.length === 1 && (<PicData_V1>resGet.ibGibs[0].data)?.binHash) {
+              picLabelIbGib = <PicIbGib_V1>resGetLatest.ibGibs[0];
+              picLabelAddr = latestpicAddr;
+            } else {
+              // some kind of error, so use best get
+              console.error(`${lc} problem getting the latest. (E: 00b7a5e2a5e34215a3f280ba7d6ac225)`);
+              picLabelIbGib = <PicIbGib_V1>resGet.ibGibs[0];
+              picLabelAddr = picAddr;
+            }
+          } else {
+            // no latest or it's the same, so just use what we got
+            picLabelIbGib = <PicIbGib_V1>resGet.ibGibs[0];
+            picLabelAddr = picAddr;
+          }
+          break;
+        } else {
+          console.error(`${lc} there was a problem loading a child pic for the label? trying next. addr: ${picAddr}. error: ${resGet.errorMsg ?? 'some error'} (E: 00f0b8cf73024c0da6dd05ee87f2bb3c)`)
+        }
+      }
+
+      const clearPicLabel = () => {
+        delete this.picLabelItem;
+      }
+
+      if (picLabelAddr && picLabelIbGib) {
+        // load the pic into an item
+        let { ib, gib } = picLabelIbGib;
+        let picLabelItem: IbGibItem = {
+          ib, gib,
+          ibGib: picLabelIbGib,
+          addr: picLabelAddr,
+        };
+        // reuse the loadPic machinery but for a different "pic label" item (not
+        // this component's `item` model.)
+        await this.loadPic(picLabelItem);
+        if (picLabelItem.picSrc) {
+          this.picLabelItem = picLabelItem;
+        } else {
+          console.error(`${lc} (UNEXPECTED) loadPic returned but item.picSrc is falsy? (E: 7ebad3d9a87149ebbe4439d3e42c08a5)`)
+          clearPicLabel();
+        }
+      } else {
+        console.error(`${lc} (UNEXPECTED) couldn't load the label for some reason. labelIbGib falsy. (E: 0037c9fab17a42e9b4cbfb0209d91a43)`);
+        clearPicLabel();
+      }
+    } catch (error) {
+      console.error(`${lc} ${error.message}`);
+      throw error;
+    } finally {
+      setTimeout(() => this.ref.detectChanges());
+      this._loadingPicLabelAddr = false;
+      if (logalot) { console.log(`${lc} complete.`); }
     }
   }
 
