b031b594633e64b9344f10069f95b2022eaf6315
William Raiford <bill.mybiz@gmail.com>
2022-11-18 11:52:59 -0600

    minor: display-bar spec reference...very minor

--- ionic-gib/src/app/common/bars/display-bar/display-bar.component.spec.ts
+++ ionic-gib/src/app/common/bars/display-bar/display-bar.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { getTestBedConfig_Component } from 'src/karma.global';
 
-import { RobbotBarComponent } from './robbot-bar.component';
+import { RobbotBarComponent } from '../robbot-bar/robbot-bar.component';
 
 describe('RobbotBarComponent', () => {
   let component: RobbotBarComponent;
